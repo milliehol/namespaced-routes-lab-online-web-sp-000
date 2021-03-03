ca0bfe882f59cadf4a47a37f389cd7a287258021
Carla Holcomb <carla.programmer@gmail.com>
2021-03-03 03:41:48 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126235547) do
+ActiveRecord::Schema.define(version: 4378297439453434343) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
